2753acb370e426f2a3b6114765052394e9d25ec5
Jorge Garcia <jorgarcia1994@gmail.com>
2021-01-17 19:46:30 -0800

    completed lattice dp

--- Project_Euler/lattice_paths.js
+++ Project_Euler/lattice_paths.js
@@ -11,29 +11,49 @@ function createMatrix(n ,m) {
   return new Array(n).fill([].slice()).map(() => new Array(m).fill(0))
 }
 
+// function latticePaths(n, m) {
+//   let matrix = createMatrix(n + 1, m + 1);
+//   console.log(matrix)
+//   let total = 0;
+//   traverseMatrix(0, 0);
+//   return total;
+
+
+//   function traverseMatrix(x, y) {
+//     if((x >= matrix.length) || (y >= matrix[0].length)) return
+//     if((x === matrix.length - 1) && (y === matrix[0].length - 1)) {
+//       total++;
+//       return
+//     };
+
+//     let directions = [[0, 1], [1, 0]];
+//     for (let [currX, currY] of directions) {
+//       let newX = currX + x;
+//       let newY = currY + y;
+//       traverseMatrix(newX, newY);
+//     }
+//   }
+// }
+
+
+
 function latticePaths(n, m) {
-  let matrix = createMatrix(n + 1, m + 1);
-  console.log(matrix)
-  let total = 0;
-  traverseMatrix(0, 0);
-  return total;
+  let matrix = createMatrix(n, m);
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[0][i] = i + 2
+    matrix[i][0] = i + 2
+  }
 
-
-  function traverseMatrix(x, y) {
-    if((x >= matrix.length) || (y >= matrix[0].length)) return
-    if((x === matrix.length - 1) && (y === matrix[0].length - 1)) {
-      total++;
-      return
-    };
-
-    let directions = [[0, 1], [1, 0]];
-    for (let [currX, currY] of directions) {
-      let newX = currX + x;
-      let newY = currY + y;
-      traverseMatrix(newX, newY);
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix.length; j++) {
+      let leftVal = matrix[i][j-1];
+      let topVal = matrix[i-1][j];
+      matrix[i][j] = leftVal + topVal;
     }
   }
+  console.log(matrix)
+  return matrix[matrix.length - 1][matrix.length - 1];
 }
 
-let sample = latticePaths(4, 2);
+let sample = latticePaths(20, 20);
 console.log(sample)
